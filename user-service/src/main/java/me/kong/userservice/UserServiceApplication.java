599bf141aedb3cf0e980470a3e783efef5d58453
khb6997@gmail.com <khb6997@gmail.com>
2024-02-14 18:48:44 +0900

    user-service 프로젝트 생성

---
+++ user-service/src/main/java/me/kong/userservice/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package me.kong.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
