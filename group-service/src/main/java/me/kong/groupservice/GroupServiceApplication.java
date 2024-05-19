4860518c98fc988bbc88724aae8f26cdb807caba
khb6997@gmail.com <khb6997@gmail.com>
2024-05-19 22:23:30 +0900

    group-service 프로젝트 생성

---
+++ group-service/src/main/java/me/kong/groupservice/GroupServiceApplication.java
@@ -0,0 +1,13 @@
+package me.kong.groupservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroupServiceApplication.class, args);
+    }
+
+}
---
+++ group-service/src/test/java/me/kong/groupservice/GroupServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kong.groupservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
