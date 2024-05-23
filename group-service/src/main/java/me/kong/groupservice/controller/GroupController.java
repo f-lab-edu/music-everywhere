f90307db5ff801796105d47899d2b466fc3e7e2d
khb6997@gmail.com <khb6997@gmail.com>
2024-05-23 21:23:04 +0900

    controller, dto, mapper 구현

--- group-service/src/main/java/me/kong/groupservice/controller/GroupController.java
+++ group-service/src/main/java/me/kong/groupservice/controller/GroupController.java
@@ -3,14 +3,19 @@ package me.kong.groupservice.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import me.kong.groupservice.domain.entity.group.Group;
+import me.kong.groupservice.dto.request.GroupJoinRequestDto;
 import me.kong.groupservice.dto.request.SaveGroupRequestDto;
 import me.kong.groupservice.dto.response.GroupResponseDto;
 import me.kong.groupservice.mapper.GroupMapper;
 import me.kong.groupservice.service.GroupService;
+import me.kong.groupservice.service.ProfileService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/groups")
 @RequiredArgsConstructor
@@ -26,4 +31,12 @@ public class GroupController {
 
         return ResponseEntity.ok(groupMapper.toDto(group));
     }
+
+    @PostMapping("{groupId}")
+    public ResponseEntity<HttpStatus> joinGroup(@PathVariable Long groupId, @RequestBody @Valid GroupJoinRequestDto dto) {
+        groupService.joinGroup(dto, groupId);
+
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
 }
