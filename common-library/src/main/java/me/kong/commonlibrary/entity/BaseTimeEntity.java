ccd4262140861ab8d362a8dc34fb11304fe3b3ab
khb6997@gmail.com <khb6997@gmail.com>
2024-05-28 01:45:36 +0900

    JPA Audit 클래스 추출

---
+++ common-library/src/main/java/me/kong/commonlibrary/entity/BaseTimeEntity.java
@@ -0,0 +1,22 @@
+package me.kong.commonlibrary.entity;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime updatedDate;
+}
