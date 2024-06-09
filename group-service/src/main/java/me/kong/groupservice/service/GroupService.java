7d5046ce6116f37008b5935f0dcbe93c2ea92f35
khb6997@gmail.com <khb6997@gmail.com>
2024-06-10 01:13:19 +0900

    외부에서 userId 주입받도록 수정

--- group-service/src/main/java/me/kong/groupservice/service/GroupService.java
+++ group-service/src/main/java/me/kong/groupservice/service/GroupService.java
@@ -3,24 +3,16 @@ package me.kong.groupservice.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import me.kong.commonlibrary.exception.auth.UnAuthorizedException;
-import me.kong.commonlibrary.exception.common.DuplicateElementException;
+import me.kong.commonlibrary.util.JwtReader;
 import me.kong.groupservice.common.exception.GroupFullException;
-import me.kong.groupservice.common.exception.NoLoggedInProfileException;
-import me.kong.groupservice.domain.entity.State;
-import me.kong.groupservice.domain.entity.group.JoinCondition;
 import me.kong.groupservice.domain.entity.profile.GroupRole;
 import me.kong.groupservice.domain.entity.group.Group;
-import me.kong.groupservice.domain.entity.profile.Profile;
 import me.kong.groupservice.domain.repository.GroupRepository;
-import me.kong.groupservice.dto.request.GroupJoinRequestDto;
 import me.kong.groupservice.dto.request.SaveGroupRequestDto;
 import me.kong.groupservice.mapper.GroupMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.NoSuchElementException;
 
 @Slf4j
@@ -31,12 +23,13 @@ public class GroupService {
     private final GroupRepository groupRepository;
     private final ProfileService profileService;
     private final GroupMapper groupMapper;
+    private final JwtReader jwtReader;
 
     @Transactional
     public Group createNewGroup(SaveGroupRequestDto dto) {
         Group group = groupRepository.save(groupMapper.toEntity(dto));
 
-        profileService.createNewProfile(dto.getNickname(), GroupRole.MANAGER, group);
+        profileService.createNewProfile(dto.getNickname(), jwtReader.getUserId(), GroupRole.MANAGER, group);
 
         return group;
     }
