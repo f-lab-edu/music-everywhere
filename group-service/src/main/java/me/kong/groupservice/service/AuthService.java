a4278d4ad24dd71d4952df89b57a74f1f93784fd
khb6997@gmail.com <khb6997@gmail.com>
2024-06-23 18:05:32 +0900

    권한 체크 기능 구현

---
+++ group-service/src/main/java/me/kong/groupservice/service/AuthService.java
@@ -0,0 +1,23 @@
+package me.kong.groupservice.service;
+
+import lombok.RequiredArgsConstructor;
+import me.kong.groupservice.domain.entity.State;
+import me.kong.groupservice.domain.entity.group.Group;
+import me.kong.groupservice.domain.entity.profile.GroupRole;
+import me.kong.groupservice.domain.entity.profile.Profile;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    public boolean isGroupMember(Group group, Profile profile) {
+        return profile.getGroup().getId().equals(group.getId())
+                && profile.getState() != State.GENERAL;
+    }
+
+    public boolean isGroupManager(Group group, Profile profile) {
+        return isGroupMember(group, profile)
+                && profile.getGroupRole() == GroupRole.MANAGER;
+    }
+}
