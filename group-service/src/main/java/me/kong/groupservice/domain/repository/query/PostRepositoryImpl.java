cf771d923eb89ef46590c21c45911cbd547c412e
khb6997@gmail.com <khb6997@gmail.com>
2024-07-13 20:48:18 +0900

    불필요한 라인 제거

--- group-service/src/main/java/me/kong/groupservice/domain/repository/query/PostRepositoryImpl.java
+++ group-service/src/main/java/me/kong/groupservice/domain/repository/query/PostRepositoryImpl.java
@@ -35,7 +35,6 @@ public class PostRepositoryImpl implements CustomPostRepository {
                         cursorIdLt(cursorId)
                 )
                 .orderBy(post.id.desc())
-                .offset(pageable.getOffset())
                 .limit(pageable.getPageSize() + 1)
                 .fetch();
 
