3a60c6610fa470e6901c10a3dc84585bbaff0997
khb6997@gmail.com <khb6997@gmail.com>
2024-07-17 20:55:15 +0900

    결제 서비스 프로젝트 생성

---
+++ payment-service/src/test/java/me/kong/paymentservice/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kong.paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
