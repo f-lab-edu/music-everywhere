2fdfc00d1a14b7af6e2cc62193e8394161832112
khb6997@gmail.com <khb6997@gmail.com>
2024-10-01 01:59:15 +0900

    feign client 의존성 추가

--- group-service/src/main/java/me/kong/groupservice/GroupServiceApplication.java
+++ group-service/src/main/java/me/kong/groupservice/GroupServiceApplication.java
@@ -2,10 +2,12 @@ package me.kong.groupservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
 @EnableJpaAuditing
+@EnableFeignClients
 @SpringBootApplication
 public class GroupServiceApplication {
 
