8a4d43d676a3f421ad4c47749be5b7a5be913288
khb6997@gmail.com <khb6997@gmail.com>
2024-10-02 20:42:00 +0900

    쿼리 성능 개선 - 너무 긴 게시글 일부분만 로드하도록

--- group-service/src/main/java/me/kong/groupservice/domain/repository/query/PostRepositoryImpl.java
+++ group-service/src/main/java/me/kong/groupservice/domain/repository/query/PostRepositoryImpl.java
@@ -2,16 +2,17 @@ package me.kong.groupservice.domain.repository.query;
 
 
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
-import me.kong.groupservice.domain.entity.post.Post;
 import me.kong.groupservice.domain.entity.post.PostScope;
 import me.kong.groupservice.dto.request.condition.PostSearchCondition;
+import me.kong.groupservice.dto.response.PostListResponseDto;
+import me.kong.groupservice.dto.response.QPostListResponseDto;
 import org.springframework.data.domain.*;
 
 import java.util.List;
 
-import static me.kong.groupservice.domain.entity.group.QGroup.*;
 import static me.kong.groupservice.domain.entity.post.QPost.*;
 import static me.kong.groupservice.domain.entity.profile.QProfile.*;
 
@@ -22,7 +23,7 @@ public class PostRepositoryImpl implements CustomPostRepository {
     private final JPAQueryFactory queryFactory;
 
     @Override
-    public Slice<Post> searchRecentPosts(Long postId, PostSearchCondition cond, Pageable pageable) {
+    public Slice<PostListResponseDto> searchRecentPosts(Long postId, PostSearchCondition cond, Pageable pageable) {
         List<Long> postIds = queryFactory
                 .select(post.id)
                 .from(post)
@@ -41,17 +42,19 @@ public class PostRepositoryImpl implements CustomPostRepository {
             hasNext = true;
         }
 
-        List<Post> content = queryFactory
-                .select(post)
+        List<PostListResponseDto> postLists = queryFactory
+                .select(new QPostListResponseDto(
+                        post.id,
+                        Expressions.stringTemplate("SUBSTRING({0}, 1, 300)", post.content),
+                        post.group.id, post.profile.id,
+                        post.profile.nickname, post.updatedDate))
                 .from(post)
-                .join(post.group, group).fetchJoin()
-                .join(post.profile, profile).fetchJoin()
+                .join(post.profile, profile)
                 .where(post.id.in(postIds))
                 .orderBy(post.id.desc())
-                .limit(pageable.getPageSize() + 1)
                 .fetch();
 
-        return new SliceImpl<>(content, pageable, hasNext);
+        return new SliceImpl<>(postLists, pageable, hasNext);
     }
 
     private BooleanExpression postIdLt(Long postId) {
